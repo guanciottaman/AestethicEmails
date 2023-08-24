7ed8df2f1d35aa92fefefddb87591ccc6a5256d9
Guanciottaman <italia.matteo00@gmail.com>
2023-08-24 15:51:32 +0200

    Update

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = b'+\xa7\x87\xf3\x83Qbs8W\xef\xf7p\x92Q\x8b'
+
+@app.route('/', methods=['GET'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
